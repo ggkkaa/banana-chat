18dcf725f48ba31d3db4184d35db1599e5f530b6
Luka <luka.talevski.hn@gmail.com>
2025-04-04 12:08:11 +0200

    Basic UI Done

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use std::cell::RefCell;
 use std::io::{self, Write, Read, Error};
 use std::net::TcpStream;
+use std::path::Path;
 use std::rc::Rc;
 use std::thread;
 
-use gtk::{prelude::*, Button, Entry, Box};
-use gtk::{glib, Application, ApplicationWindow};
+use gtk::{prelude::*, Button, Entry};
+use gtk::{glib, Application, ApplicationWindow, Builder, Window};
+use gdk::Key;
 
-
-const APP_ID: &str = "org.banan.chat";
+const APP_ID: &str = "com.bananymous.chat";
 
 fn main() -> Result<(), Error> {
     let mut stream = TcpStream::connect("88.193.139.141:6969").unwrap();
     println!("Connected to the server!");
 
     let app = Application::builder().application_id(APP_ID).build();
-    
     let gtk_stream = Rc::new(RefCell::new(stream.try_clone().unwrap()));
     app.connect_activate(move |app|  build_ui(app, gtk_stream.clone()));
 
@@ -23,7 +23,7 @@ fn main() -> Result<(), Error> {
 
     let mut out_stream = stream.try_clone().unwrap();
     let _read_thread = thread::spawn(move || {
-        let mut buffer = [0; 1024];
+        let mut buffer = [0; 1024]; 
         loop {
             match out_stream.read(&mut buffer) {
                 Ok(0) => {
@@ -55,34 +55,42 @@ fn main() -> Result<(), Error> {
 }
 
 fn build_ui(app: &Application, stream: Rc<RefCell<TcpStream>>) {
-    // Create a window and set the title
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("Banana Chat")
-        .build();
+    let ui_file = Path::new("/home/luka/Documents/banana-chat/target/debug/asssets/ui/main.ui");
+    let builder = Builder::from_file(ui_file);
 
-        let hbox = Box::new(gtk::Orientation::Horizontal, 10);
-        
-        let entry = Entry::new();
-        entry.set_placeholder_text(Some("Message ..."));
-        let stream_clone = stream.clone();
+    let window: Window = builder.object("window").expect("Failed to get window from UI file");
+    window.set_decorated(true);
+    window.set_default_width(800);
+    window.set_default_height(600);
 
-    let entry_clone = entry.clone();
-        let button = Button::with_label("Send");
-        button.connect_clicked(move |_| {
-            let text = entry_clone.text();
-            let mut stream = stream_clone.borrow_mut();
-            if let Err(err) = stream.write_all(text.as_bytes()) {
-                eprintln!("Failed to send message: {}", err);
+    let button: Button = builder.object("send_button").expect("Failed to get button from UI file");
+    let send_field: Entry = builder.object("entry").expect("Failed to get entry");
+
+    let event_controller = gtk::EventControllerKey::new();
+
+    let event_send_field = send_field.clone();
+
+    let stream_event = stream.clone();
+
+    event_controller.connect_key_pressed(move |_, key, _, _| {
+        match key {
+            Key::Return => {
+                let mut stream_event = stream_event.borrow_mut();
+
+                stream_event.write_all(event_send_field.text().as_bytes()).unwrap();
             }
-            entry_clone.set_text("");
-        });
+            _ => {}
+        }
+        glib::Propagation::Proceed
+    });
 
-        hbox.append(&entry);
-        hbox.append(&button);
 
-        window.set_child(Some(&hbox));
+    button.connect_clicked(move |_| {    
+        let mut stream_borrow = stream.borrow_mut();
+        stream_borrow.write_all(send_field.text().as_bytes()).unwrap();
+    });
 
-    // Present window
+    window.set_application(Some(app));
+
     window.present();
 }
