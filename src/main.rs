22c768037ca80c96bbb127e0a502a1446b8b3e3f
Luka <luka.talevski.hn@gmail.com>
2025-04-05 08:18:29 +0200

    Add basic text features

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,17 @@ use std::net::TcpStream;
 use std::path::Path;
 use std::rc::Rc;
 use std::thread;
+use std::time::Duration;
 
-use gtk::{prelude::*, Button, Entry};
+use gtk::{prelude::*, Button, Entry, Label, ListBox};
 use gtk::{glib, Application, ApplicationWindow, Builder, Window};
 use gdk::Key;
 
 const APP_ID: &str = "com.bananymous.chat";
 
 fn main() -> Result<(), Error> {
-    let mut stream = TcpStream::connect("88.193.139.141:6969").unwrap();
+    let stream = TcpStream::connect("88.193.139.141:6969").unwrap();
+    stream.set_nonblocking(true)?;
     println!("Connected to the server!");
 
     let app = Application::builder().application_id(APP_ID).build();
@@ -21,37 +23,7 @@ fn main() -> Result<(), Error> {
 
     app.run();
 
-    let mut out_stream = stream.try_clone().unwrap();
-    let _read_thread = thread::spawn(move || {
-        let mut buffer = [0; 1024]; 
-        loop {
-            match out_stream.read(&mut buffer) {
-                Ok(0) => {
-                    println!("Server closed the connection.");
-                    break;
-                }
-                Ok(n) => {
-                    let message = String::from_utf8_lossy(&buffer[..n]);
-                    print!("{}", message);
-                    io::stdout().flush().unwrap();
-                }
-                Err(e) => {
-                    eprintln!("Error reading from server: {}", e);
-                    break;
-                }
-                
-            }
-        }
-    });
-
-
-    let mut input = String::new();
-
-    loop {
-        io::stdin().read_line(&mut input).unwrap();
-        stream.write_all(input.as_bytes()).unwrap();
-        input.clear();
-    }
+    Result::Ok(())
 }
 
 fn build_ui(app: &Application, stream: Rc<RefCell<TcpStream>>) {
@@ -65,6 +37,12 @@ fn build_ui(app: &Application, stream: Rc<RefCell<TcpStream>>) {
 
     let button: Button = builder.object("send_button").expect("Failed to get button from UI file");
     let send_field: Entry = builder.object("entry").expect("Failed to get entry");
+    let chatlist: ListBox = builder.object("chatlist").expect("Failed to get chatlist from UI file");
+    
+    let join_msg = Label::new(Some("Welcome to Banana Chat!"));
+    join_msg.set_xalign(0.0);
+
+    chatlist.append(&join_msg);
 
     let event_controller = gtk::EventControllerKey::new();
 
@@ -84,10 +62,37 @@ fn build_ui(app: &Application, stream: Rc<RefCell<TcpStream>>) {
         glib::Propagation::Proceed
     });
 
+    send_field.add_controller(event_controller);
 
+    let stream_button = stream.clone();
     button.connect_clicked(move |_| {    
-        let mut stream_borrow = stream.borrow_mut();
-        stream_borrow.write_all(send_field.text().as_bytes()).unwrap();
+        let mut stream_button = stream_button.borrow_mut();
+        stream_button.write_all(send_field.text().as_bytes()).unwrap();
+        send_field.set_text("");
+    });
+
+    glib::timeout_add_local(Duration::from_millis(500), move || {
+        let mut stream_timeout = stream.borrow_mut();
+
+        let mut buf = [0u8; 1024];
+
+        match stream_timeout.read(&mut buf) {
+            Ok(0) => {
+                println!("Server closed the connection.");
+                return glib::ControlFlow::Break;
+            }
+            Ok(n) => {
+                let text = String::from_utf8_lossy(&buf[..n]);
+                let msg = Label::new(Some(&text));
+                msg.set_xalign(0.0);
+                chatlist.append(&msg);
+            }
+            Err(e) => {
+                eprintln!("Error reading from server: {}", e);
+            }
+        }
+
+        glib::ControlFlow::Continue
     });
 
     window.set_application(Some(app));
